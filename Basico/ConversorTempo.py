108a9e2b5c7c5241d96bfdb045a72c8977caad09
area-41 <email.diferente.victor@gmail.com>
2023-07-19 13:38:15 -0300

    Commit com ajustes e tstes de varios horarios no Conversor de Horas code

--- Basico/ConversorTempo.py
+++ Basico/ConversorTempo.py
@@ -4,24 +4,26 @@ def timeConversion(s):
         x = s.split(':')
         if x[0] == '12':
             x[2] = x[2][0:2]
-            resultado = str(x[0])+":"+str(x[1])+":"+str(x[2])
-            return resultado
+            time_converted = str(x[0])+":"+str(x[1])+":"+str(x[2])
+            return time_converted
         x[0] = int(x[0]) + 12
         x[2] = x[2][0:2]
-        resultado = str(x[0])+":"+str(x[1])+":"+str(x[2])
-        return resultado
+        time_converted = str(x[0])+":"+str(x[1])+":"+str(x[2])
+        return time_converted
     if s[-2] == 'A':
         x = s.split(':')
         x[2] = x[2][0:2]
         if x[0] == '12':
             x[0] = '00'
-            resultado = str(x[0])+":"+str(x[1])+":"+str(x[2])
-            return resultado
-        resultado = str(x[0])+":"+str(x[1])+":"+str(x[2])
-        return resultado
+            time_converted = str(x[0])+":"+str(x[1])+":"+str(x[2])
+            return time_converted
+        time_converted = str(x[0])+":"+str(x[1])+":"+str(x[2])
+        return time_converted
 
 
 if __name__ == '__main__':
-    s = '12:05:45AM'
-    result = timeConversion(s)
-    print(result)
+    print(timeConversion('12:05:45AM'))
+    print(timeConversion('02:34:20AM'))
+    print(timeConversion('00:01:05PM'))
+    print(timeConversion('01:01:05PM'))
+    print(timeConversion('10:58:55PM'))
