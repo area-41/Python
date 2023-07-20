b8804f1e1e502783bd293e93d42f58e8f08baa30
area-41 <email.diferente.victor@gmail.com>
2023-07-20 13:50:33 -0300

    Commit Arredondador de Notas code

--- Basico/arredondadorNotas.py
+++ Basico/arredondadorNotas.py
@@ -8,13 +8,16 @@ A Universidade tem a seguinte política de notas:
         - Se o valor da nota for menor que 38, nenhum arredondamento ocorrerá, pois o resultado ainda será uma nota de reprovação.
 """
 
+
 def gradingStudents(grades):
     for i in range(0, len(grades)):
         if grades[i] < 38:
             continue
         else:
             temp = grades[i]  # pega na lista o elemento da ordem
-            grades_temp = temp % 5  # verifica o resto da divisao por 5, se a diferença para 5 for menor de 3, ou seja, se o numero for 3 ou 4
+            grades_temp = temp % 5
+            # verifica o resto da divisao por 5, se a diferença para 5 for menor de 3
+            # ou seja, se o numero for 3 ou 4
             if grades_temp == 3:
                 temp = temp + 2  # se for 3 adiciona 2
                 grades[i] = temp
@@ -27,7 +30,7 @@ def gradingStudents(grades):
 
 
 if __name__ == '__main__':
-    list = [4, 73, 67, 38, 33]
-    print(list)
+    list = [4, 73, 69, 38, 33, 44, 58, 77, 39, 34]
+    print("Lista oiginal: \n\t", list)
     student = gradingStudents(list)  # lista com  4 notas
-    print(f"Notas arredondadas segundo os critérios: \n {student}".format(list, student))
+    print(f"Notas arredondadas segundo os critérios: \n\t{student}".format(student))
