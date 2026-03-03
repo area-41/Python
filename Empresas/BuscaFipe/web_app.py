8696374176fff62b3819b7faecf8f3335bb8efb3
Victor Marques <87396846+area-41@users.noreply.github.com>
2026-03-03 16:12:58 -0300

    Simplify marcas_df filtering logic
    Removed unnecessary filtering of marcas_df to simplify code.

--- Empresas/BuscaFipe/web_app.py
+++ Empresas/BuscaFipe/web_app.py
@@ -18,9 +18,7 @@ def main():
                                  list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
     marcas_df = api.listar_marcas()
-    # Filtra ignorando espaços antes do nome e garantindo que é maiúsculo
-    marcas_df = marcas_df[marcas_df['nome'].str.strip().str.upper().str.startswith(letra)]
-
+    
     if not marcas_df.empty:
         # Filtragem por letra
         marcas_filtradas = marcas_df[marcas_df['nome'].str.startswith(letra)]
